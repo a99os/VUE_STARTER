9ee2dde1d7b7d6c18bc2f1024b4bd33f5aea4bea
a99os <abbosashirboev@gmail.com>
2023-02-14 13:56:14 +0500

    add vue starter

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template lang="">
+  <h1 class="bg-green-400">Salom</h1>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "./assets/main.css";
+// import "./index.css";
+import "./output.css";
+
+createApp(App).mount("#app");
