7c056f02e26d8fc912d809da0dbceee17ef40022
a99os <abbosashirboev@gmail.com>
2023-02-28 09:33:10 +0500

    add good version starter

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
-<template lang="">
-  <h1 class="bg-green-400">Salom</h1>
-</template>
-<script>
-export default {};
-</script>
-<style lang=""></style>
+<template></template>
+<script></script>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template lang="">
+  <div class="bg-green-400  ">Salom</div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss"></style>
